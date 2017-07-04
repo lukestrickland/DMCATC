0372af049b978a16182d82bce8943a86d7b72148
lukestrickland <luke.strickland@utas.edu.au>
2017-07-04 13:19:44 +1000

    Revert "Hierarchical sampling file updated for Russ's model-" Reverted because we accidentally tracked an RDatatmp. This reverts commit acfddd48d621af7509c4101fc85faf9335699e3a.

--- Hierarchical_Sampling_A1_A2.R
+++ Hierarchical_Sampling_A1_A2.R
@@ -1,59 +1,67 @@
 rm(list=ls()) 
-setwd("D:/Software/DMC_ATCPMDC")
 source ("dmc/dmc.R")
-load_model ("LBA","lbaN_B.R")
-load("data/samples/A1.block.B.V_cond.B.V.PMV.samples.RData")
-cores=64
+load_model ("LBA","lba_BcsuPPT.R")
+load("rscsuPPT.RData")
+cores=30
 
-load("data/exp_data/A1.block.B.V_cond.B.V.PMV.data.model.RData")
+# STAGE 1
+csuPPT <- h.RUN.dmc(csuPPT,cores=32,verbose=TRUE)
+save(dm,csuPPT,file="rscsuPPT.RData")
 
-dm <- A1.block.B.V_cond.B.V.PM
-
-A1 <- A1.block.B.V_cond.B.V.PMV.samples
-
-p.prior <- A1[[1]]$p.prior
-p1 <- get.p.vector(A1[[1]])[names(p.prior)]
+# Make hierarchical
+p.prior <- csuPPT[[1]]$p.prior
+p1 <- get.p.vector(csuPPT[[1]])[names(p.prior)]
 s.prior <- prior.p.dmc(p1=p1,p2=p1,
   dists=rep("gamma",length(p1)))
 pp.prior=list(p.prior,s.prior)
 
-hstart <- make.hstart(A1)
-theta1 <- make.theta1(A1)
-hA1 <- 
-  h.samples.dmc(nmc=120,p.prior,dm,pp.prior,
-  hstart.prior=hstart,theta1=theta1,thin=20)
+hstart <- make.hstart(csuPPT)
+theta1 <- make.theta1(csuPPT)
+hcsuPPT <- h.samples.dmc(nmc=100,p.prior,dm,pp.prior,
+  hstart.prior=hstart,theta1=theta1,thin=10)
 
 # STAGE 2
-hA1  <- h.run.unstuck.dmc(hA1, p.migrate = .05, cores = cores)
-save(A1,hA1,file="rsA1.RData")
-hA1 <- h.run.converge.dmc(h.samples.dmc(nmc=120, samples=hA1), 
-  thorough=TRUE,cores=cores,finalrun=TRUE,finalI=120)
-save(A1,hA1,file="rsA1.RData")
+hcsuPPT  <- h.run.unstuck.dmc(hcsuPPT, p.migrate = .05, cores = cores)
+save(csuPPT,hcsuPPT,file="rscsuPPT.RData")
+hcsuPPT <- h.run.converge.dmc(h.samples.dmc(nmc=100, samples=hcsuPPT), 
+  thorough=TRUE,nmc=50,cores=cores,finalrun=TRUE,finalI=500)
+save(csuPPT,hcsuPPT,file="rscsuPPT.RData")
+# hcsuPPT <- h.run.dmc(h.samples.dmc(samples=hcsuPPT,nmc=2000),cores=cores,report=100)
+# save(csuPPT,hcsuPPT,file="rscsuPPT.RData")
+# hcsuPPT <- h.run.dmc(h.samples.dmc(samples=hcsuPPT,nmc=2000),cores=cores,report=100)
+# save(csuPPT,hcsuPPT,file="rscsuPPT.RData")
+# hcsuPPT <- h.run.dmc(h.samples.dmc(samples=hcsuPPT,nmc=500),cores=cores,report=100)
+# save(csuPPT,hcsuPPT,file="rscsuPPT.RData")
 
 # POST PROCESS
 
-pdf("rshA1_chains.pdf",height=6,width = 8)
-plot.dmc(hA1,hyper=TRUE,pll.chain=TRUE) 
-plot.dmc(hA1,hyper=TRUE,layout=c(2,5))
-plot.dmc(hA1,hyper=TRUE,layout=c(2,5),p.prior=pp.prior)
+pdf("rshcsuPPT_chains.pdf",height=6,width = 8)
+plot.dmc(hcsuPPT,hyper=TRUE,pll.chain=TRUE) 
+plot.dmc(hcsuPPT,hyper=TRUE,layout=c(2,5))
+plot.dmc(hcsuPPT,hyper=TRUE,layout=c(2,5),p.prior=pp.prior)
 dev.off()
 
-ppA1 <- h.post.predict.dmc(hA1,cores=32)
-save(A1,hA1,ppA1,file="rsA1.RData")
-pdf("rshA1_fit.pdf",height=6, width = 8)
-plot.pp.dmc(ppA1,model.legend = FALSE,layout=c(2,3))
+ppcsuPPT <- h.post.predict.dmc(hcsuPPT,cores=32)
+save(csuPPT,hcsuPPT,ppcsuPPT,file="rscsuPPT.RData")
+pdf("rshcsuPPT_fit.pdf",height=6, width = 8)
+plot.pp.dmc(ppcsuPPT,model.legend = FALSE,layout=c(2,3))
 dev.off()
 
-A1.pll <- group_trial_log_likes(hA1,thin_pointwise = 10,cores=16)
-A1.waic <- waic.dmc(A1.pll,digits=2,save=TRUE)
-save(A1,hA1,ppA1,A1.waic,file="rsA1.RData")
-h.IC.dmc(hA1,DIC=TRUE)
+csuPPT.pll <- group_trial_log_likes(hcsuPPT,thin_pointwise = 10,cores=16)
+csuPPT.waic <- waic.dmc(csuPPT.pll,digits=2,save=TRUE)
+save(csuPPT,hcsuPPT,ppcsuPPT,csuPPT.waic,file="rscsuPPT.RData")
+h.IC.dmc(hcsuPPT,DIC=TRUE)
 
 
 
-gelman.diag.dmc(hA1,hyper=TRUE)
-gelman.diag.dmc(hA1)
+gelman.diag.dmc(hcsuPPT,hyper=TRUE)
+gelman.diag.dmc(hcsuPPT)
 
-tmp <- summary.dmc(hA1,hyper=TRUE)$quantiles
+tmp <- summary.dmc(hcsuPPT,hyper=TRUE)$quantiles
 round(tmp[,c(1,3,5)],3)
 
+
+# pdf("rscsuPPT_fits.pdf",height=10,width = 12)
+# for (i in 1:length(hcsuPPT))
+#   plot.pp.dmc(ppcsuPPT[[i]],"cdf",model.legend = FALSE,layout=c(5,6))
+# dev.off()
